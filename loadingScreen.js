6694d5c45fb171b63f09d6d6720d359e6f7f6381
Poseidon <63670929+poseidon-boi@users.noreply.github.com>
2025-05-16 17:56:11 +0530

    Changed the loader and the positions of the images in the alumni page

--- loadingScreen.js
+++ loadingScreen.js
@@ -1,22 +1,29 @@
-const loaderContainer = document.getElementById('loader-container');
+// const loaderContainer = document.getElementById("loader-container");
 
-if (!loaderContainer) {
-    console.error('Loader container element not found');
-} else {
-    let loaderElement = document.createElement('img');
-    loaderElement.id = 'loader-video';
-    loaderElement.style.zIndex = 20000;
-    loaderElement.src = 'loadingAnim/loading.gif';
+// if (!loaderContainer) {
+// 	console.error("Loader container element not found");
+// } else {
+// 	let loaderElement = document.createElement("img");
+// 	loaderElement.id = "loader-video";
+// 	loaderElement.style.zIndex = 20000;
+// 	loaderElement.src = "loadingAnim/loading.gif";
 
-    loaderElement.addEventListener('load', () => {
-        hideLoader();
-    });
+// 	loaderElement.addEventListener("load", () => {
+// 		hideLoader();
+// 	});
 
-    function hideLoader() {
-        if (loaderContainer) {
-            loaderContainer.parentNode.removeChild(loaderContainer);
-        }
-    }
+// 	function hideLoader() {
+// 		if (loaderContainer) {
+// 			loaderContainer.parentNode.removeChild(loaderContainer);
+// 		}
+// 	}
 
-    loaderContainer.appendChild(loaderElement);
-}
+// 	loaderContainer.appendChild(loaderElement);
+// }
+
+document.addEventListener("readystatechange", (event) => {
+	if (event.target.readyState === "complete") {
+		console.log("readyState: Complete");
+		document.getElementById("loading-screen").style.display = "none";
+	}
+});
