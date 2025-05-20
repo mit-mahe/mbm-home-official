5725925b929c3f1c28c93b5da81d3c62cff12f85
Poseidon <63670929+poseidon-boi@users.noreply.github.com>
2025-05-21 04:13:18 +0530

    Fixed up the way the project page animations work

--- javascript/animation.js
+++ javascript/animation.js
@@ -9,18 +9,11 @@ function initApp() {
 	console.log(animation);
 	animation.onended = (event) => {
 		console.log("Animation ended");
-		let maxWidthQuery = window.matchMedia("(max-width: 1024px)");
-		if (maxWidthQuery.matches) {
-			animation.style.maxWidth = "90vw";
-		} else {
-			animation.style.maxWidth = "40vw";
-		}
 		// animation.style.display = "inline-block";
 		animationContainer.classList.add("theDarkCanvas");
 		animationContainer.classList.add("darkTheme");
 		document.documentElement.style.setProperty("--HEADER-BGCOLOR", "#344546");
 		document.documentElement.style.setProperty("--HEADER-COLOR", "#f8f5ee");
-		animationContainer.classList.add("translateDown");
 		animationContent.style.display = "inline-block";
 		// animationContent.classList.add("slideIn");
 	};
