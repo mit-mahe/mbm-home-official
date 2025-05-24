060f2b911d723754235f773457c3844044d24d6c
Poseidon <63670929+poseidon-boi@users.noreply.github.com>
2025-05-24 23:40:43 +0530

    Home page content now slides in only once

--- javascript/contentSlideIn.js
+++ javascript/contentSlideIn.js
@@ -4,8 +4,6 @@ const contentObserver = new IntersectionObserver((entries) => {
 	entries.forEach((entry) => {
 		if (entry.isIntersecting) {
 			entry.target.classList.add("show");
-		} else {
-			entry.target.classList.remove("show");
 		}
 	});
 });
